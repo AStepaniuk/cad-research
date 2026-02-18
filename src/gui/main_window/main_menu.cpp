89baeb7867fea251c4e650dc4b5fcae335e66378
Aleksander Stepaniuk <AStepanjuk@gmail.com>
2026-02-18 13:22:34 +0300

    Main menu is dynamically generated

--- src/gui/main_window/main_menu.cpp
+++ src/gui/main_window/main_menu.cpp
@@ -3,41 +3,75 @@
 #include <iostream>
 #include <imgui.h>
 
+#include "overloaded.h"
+
 using namespace gui;
+
+main_menu::main_menu()
+{
+    _structure = 
+    {
+        menu_meta
+        {
+            .caption = "File",
+            .items =
+            {
+                menu_item_meta 
+                {
+                    .caption = "Open"
+                },                
+                menu_item_meta 
+                {
+                    .caption = "Save"
+                },
+                separator_meta {},
+                menu_item_meta 
+                {
+                    .caption = "Exit"
+                }
+            }
+        },
+        menu_meta
+        {
+            .caption = "Draw",
+            .items = 
+            {
+                menu_item_meta
+                {
+                    .caption = "Wall",
+                    .hotkey = ImGuiKey_W,
+                    .item_choice = item::add_wall
+                }
+            }
+        }
+    };
+
+    process_structure(_structure);
+}
 
 void main_menu::process_frame()
 {
     _choosen_item = item::none;
 
+    if (!ImGui::GetIO().WantCaptureKeyboard) 
+    {
+        for (const auto& hotkey_pair : _hotkey_items)
+        {
+            if (ImGui::IsKeyPressed(hotkey_pair.first))
+            {
+                _choosen_item = hotkey_pair.second;
+                break;
+            }
+        } 
+    }
+
     if (ImGui::BeginMainMenuBar())
     {
         _is_mouse_hovering = ImGui::IsAnyItemHovered() || ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows);
 
-        if (ImGui::BeginMenu("File"))
+        for (const auto& mm : _structure)
         {
-            if (ImGui::MenuItem("Open", "Ctrl+O"))
-            {
-                // Handle open
-            }
-            if (ImGui::MenuItem("Save", "Ctrl+S"))
-            {
-                // Handle save
-            }
-            ImGui::Separator();
-            if (ImGui::MenuItem("Exit"))
-            {
-                // glfwSetWindowShouldClose(window, GLFW_TRUE);
-            }
-            ImGui::EndMenu();
-        }
-
-        if (ImGui::BeginMenu("Draw"))
-        {
-            if (ImGui::MenuItem("Wall", "W"))
-            {
-                _choosen_item = item::add_wall;
-            }
-            ImGui::EndMenu();
+            process_menu_meta_frame(mm);
         }
 
         ImGui::EndMainMenuBar();
@@ -52,4 +86,72 @@ bool main_menu::is_mouse_hovering() const
 gui::main_menu::item gui::main_menu::choosen_item() const
 {
     return _choosen_item;
+}
+
+void gui::main_menu::process_menu_meta_frame(const menu_meta &mm)
+{
+    if (ImGui::BeginMenu(mm.caption.c_str()))
+    {
+        for (const auto& menu_item : mm.items)
+        {
+            std::visit(corecad::util::overloaded
+                {
+                    [this] (const menu_item_meta& mim)
+                    {
+                        if (ImGui::MenuItem(mim.caption.c_str(), mim.hotkey_text.c_str()))
+                        {
+                            _choosen_item = mim.item_choice;
+                        }
+                    },
+                    [this] (const menu_meta& mm)
+                    {
+                        process_menu_meta_frame(mm);
+                    },
+                    [this] (const separator_meta&)
+                    {
+                        ImGui::Separator();
+                    }
+                },
+                menu_item
+            );
+        };
+        ImGui::EndMenu();
+    }
+}
+
+void main_menu::process_structure(std::vector<menu_meta> &initial_data)
+{
+    for (auto& menu_item : initial_data)
+    {
+        process_structure_items(menu_item.items);
+    }
+}
+
+void gui::main_menu::process_structure_items(std::vector<menu_meta::item_type>& initial_items)
+{
+    for (auto& menu_item : initial_items)
+    {
+        std::visit(corecad::util::overloaded
+            {
+                [this] (menu_item_meta& mim)
+                {
+                    if (mim.hotkey)
+                    {
+                        mim.hotkey_text = ImGui::GetKeyName(mim.hotkey.value());
+
+                        if (mim.item_choice != item::none)
+                        {
+                            _hotkey_items.push_back({ mim.hotkey.value(), mim.item_choice });
+                        }
+                    }
+                },
+                [this] (menu_meta& mm)
+                {
+                    process_structure_items(mm.items);
+                },
+                [this] (separator_meta&) { }
+            },
+            menu_item
+        );
+    };
 }
--- src/gui/main_window/main_menu.h
+++ src/gui/main_window/main_menu.h
@@ -1,10 +1,19 @@
 #pragma once
 
+#include <imgui.h>
+
+#include <string>
+#include <vector>
+#include <optional>
+#include <variant>
+
 namespace gui {
     class main_menu
     {
     public:
         enum class item { none, add_wall };
+
+        main_menu();
 
         void process_frame();
 
@@ -15,5 +24,33 @@ namespace gui {
     private:
         bool _is_mouse_hovering = false;
         item _choosen_item = item::none;
+
+
+        struct menu_item_meta
+        {
+            std::string caption;
+            std::optional<ImGuiKey> hotkey;
+            std::string hotkey_text;
+
+            item item_choice = item::none;
+        };
+
+        struct separator_meta {};
+
+        struct menu_meta
+        {
+            std::string caption;
+
+            using item_type = std::variant<menu_item_meta, separator_meta, menu_meta>;
+            std::vector<item_type> items;
+        };
+
+        std::vector<menu_meta> _structure;
+        std::vector<std::pair<ImGuiKey, item>> _hotkey_items;
+
+        void process_menu_meta_frame(const menu_meta& mm);
+
+        void process_structure(std::vector<menu_meta>& initial_data);
+        void process_structure_items(std::vector<menu_meta::item_type>& initial_items);
     };
 }
