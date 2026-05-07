04071f830fa7db5a189edde1c4825c925e96ca0a
Aleksander Stepaniuk <AStepanjuk@gmail.com>
2026-05-07 14:00:07 +0300

    Improved v/h snap generation

--- src/gui/editor/snap/vh_snap_builder.cpp
+++ src/gui/editor/snap/vh_snap_builder.cpp
@@ -1,5 +1,7 @@
 #include "vh_snap_builder.h"
 
+#include "views_take_constraints.h"
+
 using namespace gui::editor::snap;
 using namespace corecad::model;
 using namespace corecad::model::constraint;
@@ -21,6 +23,11 @@ void vh_snap_builder::calculate_snaps(float view_pos_x, float view_pos_y)
     const auto model_pos = _floor_view.to_model(view_pos_x, view_pos_y);
     const auto tol = _floor_view.model_interaction_tolerance();
 
+    const wall_axis_point* x_ref_point = nullptr;
+    const wall_axis_point* y_ref_point = nullptr;
+    double dx = 0.0;
+    double dy = 0.0;
+
     // for (const auto spid : _document.selected_handles)
     for (const auto pair : _document.model.data().items<wall_axis_point>())
     {
@@ -34,24 +41,42 @@ void vh_snap_builder::calculate_snaps(float view_pos_x, float view_pos_y)
 
         if (model_pos.x > p.x - tol.x && model_pos.x < p.x + tol.x)
         {
-            auto constraint = floor::constraint_t::create<floor::offset_wall_axis_point_t>(
-                spid, _document.active_handle.value(),
-                0.0, offset_direction::horizontal
-            );
-
-            const auto rank = std::abs(model_pos.x - p.x);
-            _document.active_wall_snaps.add(std::move(constraint), rank, spid);
+            if (!x_ref_point || (std::abs(model_pos.y - p.y) < dy))
+            {
+                dy = std::abs(model_pos.y - p.y);
+                x_ref_point = &p;
+            }
         }
-        
+
         if (model_pos.y > p.y - tol.y && model_pos.y < p.y + tol.y)
         {
-            auto constraint = floor::constraint_t::create<floor::offset_wall_axis_point_t>(
-                spid, _document.active_handle.value(),
-                0.0, offset_direction::vertical
-            );
-
-            const auto rank = std::abs(model_pos.y - p.y);
-            _document.active_wall_snaps.add(std::move(constraint), rank, spid);
+            if (!y_ref_point || (std::abs(model_pos.x - p.x) < dx))
+            {
+                dx = std::abs(model_pos.x - p.x);
+                y_ref_point = &p;
+            }
         }
-    }   
+    }
+
+    if (x_ref_point)
+    {
+        auto constraint = floor::constraint_t::create<floor::offset_wall_axis_point_t>(
+            x_ref_point->index, _document.active_handle.value(),
+            0.0, offset_direction::horizontal
+        );
+
+        const auto rank = std::abs(model_pos.x - x_ref_point->x);
+        _document.active_wall_snaps.add(std::move(constraint), rank, x_ref_point->index);
+    }
+
+    if (y_ref_point)
+    {
+        auto constraint = floor::constraint_t::create<floor::offset_wall_axis_point_t>(
+            y_ref_point->index, _document.active_handle.value(),
+            0.0, offset_direction::vertical
+        );
+
+        const auto rank = std::abs(model_pos.y - y_ref_point->y);
+        _document.active_wall_snaps.add(std::move(constraint), rank, y_ref_point->index);
+    }
 }
